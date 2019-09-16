4721151aa924fbb2be554a5a49e217864f24c855
oumao <evansotis2015@gmail.com>
2019-09-17 02:52:23 +0300

    Functionality added

--- checklist/views.py
+++ checklist/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Task
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'checklist/index.html')
+    tasks = Task.objects.order_by("id")
+
+    con = {'tasks': tasks}
+
+    return render(request, 'index.html', con)
+
+
+def add(request):
+    item = Task(name=request.POST["name"])
+    item.save()
+    return redirect("/")
+
+
+def delete(request):
+    item = Task.objects.get(id=request.POST["id"])
+    if item:
+        item.delete()
+    return redirect("/")
